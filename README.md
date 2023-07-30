a185330f9df91eda00bdad8a4470dfd5bc6e2706
myrdiaclonix <myrdiaclonix@proton.me>
2023-07-30 19:58:44 -0300

    chore(game): game finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 <h1 align="center">Snake game with pygame</h1>
+
+To launch the game, you'll need `python` installed whith `pygame` package, and run the `main.py` file. You can do this by running `python main.py` on terminal. Have fun! 🤗
+
+This game is made with [Pygame](https://www.pygame.org/news).
--- main.py
+++ main.py
@@ -46,5 +46,105 @@ change_to = direction
 # Initial score
 score = 0
 
+# Displaying score
+
+
+def show_score(color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+    score_rect = score_surface.get_rect()
+
+    game_window.blit(score_surface, score_rect)
+
+# Game over function
+
+
+def game_over():
+    my_font = pygame.font.SysFont('sitkabanner', 50)
+    game_over_surface = my_font.render(
+        'Your Score is : ' + str(score), True, red)
+    game_over_rect = game_over_surface.get_rect()
+
+    # Set position of the text
+    game_over_rect.midtop = (x_size / 2, y_size / 4)
+    game_window.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+
+    # Quit the program after 2 seconds
+    time.sleep(2)
+
+    # Deactive pygame library
+    pygame.quit()
+
+    quit()
+
+
 while True:
+    # Key events
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+
+    # Prevent snake go to oposite directions
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+
+    # Moving the snake
+    if direction == 'UP':
+        snake_position[1] -= 10
+    if direction == 'DOWN':
+        snake_position[1] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'RIGHT':
+        snake_position[0] += 10
+
+    snake_body.insert(0, list(snake_position))
+    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+        score += 10
+        fruit_spawn = False
+    else:
+        snake_body.pop()
+
+    if not fruit_spawn:
+        fruit_position = [random.randrange(1, (x_size // 10)) * 10,
+                          random.randrange(1, (y_size // 10)) * 10]
+
+    fruit_spawn = True
+    game_window.fill(black)
+
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green,
+                         pygame.Rect(pos[0], pos[1], 10, 10))
+
+    pygame.draw.rect(game_window, white, pygame.Rect(
+        fruit_position[0], fruit_position[1], 10, 10))
+
+    # Game over rules
+    if snake_position[0] < 0 or snake_position[0] > x_size - 10:
+        game_over()
+    if snake_position[1] < 0 or snake_position[1] > y_size - 10:
+        game_over()
+
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            game_over()
+
+    show_score(white, 'sitkabanner', 20)
+
+    # Refresh game screen
     pygame.display.update()
+    fps.tick(snake_speed)
